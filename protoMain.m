ad707e5dd42305a2a8b3a070b8aec801ab2c72e6
hungrymonkey <boringmonkey@gmail.com>
2016-08-24 16:28:48 -0700

    Add files via upload
    I only manage to get a basic map working. I am trying to get the mapping data from osm database so it include things like cafe etc. However, I cannot seem to find the right service

--- protoMain.m
+++ protoMain.m
@@ -1,12 +1,25 @@
-file_id = fopen('uber-raw-data-jun14.csv');
+file_id = fopen('uber-raw-data-apr14.csv');
+OSM_url = 'http://geoint.nrlssc.navy.mil/osm/wms/OSM/basemap/Mapnik/OSM_BASEMAP?';
+new_york_centroid = [40.6639206199602, -73.9383529238219];
+%% centroid obtained from https://www.maptechnica.com/city-map/New+York/NY/3651000
 map_database = 'Open Street Map';
-map_limits.lat = [39.4,41.7];
-map_limits.lon = [-75,-73];
+map_limits.lat = [new_york_centroid(1)-.7,new_york_centroid(1)+.7];
+map_limits.lon = [new_york_centroid(2)-.7,new_york_centroid(2)+.7];
 
 data_title =  textscan( file_id,'%s %s %s %s',1,'Delimiter', ',');
-uber_data =  textscan( file_id,'%s %f %f %s','Delimiter', ',');
-
-fig = figure
+uber_data =  textscan( file_id,'%s %f %f %s',7500,'Delimiter', ',');
+lat = uber_data{2};
+lon = uber_data{3};
+%layers = wmsfind('osm*basemap','Latlim',map_limits.lat, 'Lonlim', map_limits.lon);
+%poi_map = layers.refine('poi', 'SearchField', 'layertitle');
+wms = wmsinfo(OSM_url);
+layer = wms.Layer(1);
+%layer = layers;
+fig = figure;
 usamap(map_limits.lat, map_limits.lon);      % limit to New York area
-wmsread
+[A,R] = wmsread(layer,'Latlim',map_limits.lat, 'Lonlim', map_limits.lon);
+geoshow(A,R);
+hold on;
+geoshow(lat,lon, 'DisplayType', 'point');
+%wmsread
 fclose(file_id);
